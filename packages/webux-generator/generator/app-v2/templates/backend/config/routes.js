62b3de12425947ee20abcffd6f0d0ea86fdb5cec
tommy gingras <tommy@studiowebux.com>
2022-04-20 21:57:45 -0400

    feat: updated configs

--- packages/webux-generator/generator/app-v2/templates/backend/config/routes.js
+++ packages/webux-generator/generator/app-v2/templates/backend/config/routes.js
@@ -1,18 +1,33 @@
-const Webux = require("@studiowebux/app"); // to access the Webux.query() function
 const path = require('path');
 
-module.exports = {
-  "/": {
-    resources: {
-      "/healthcheck": [
-        {
-          method: "get",
-          middlewares: [], // By default, this route is publicly available, you should create a middleware to protect this resource.
-          action: (req, res, next) => {
-            return res.success({ msg: "Pong !" });
-          }
-        }
-      ]
-    }
-  }
-};
+module.exports = (Webux) => ({
+  routes: {
+    '/': {
+      resources: {
+        '/': [
+          {
+            method: 'get',
+            middlewares: [], // By default, this route is publicly available, you should create a middleware to protect this resource.
+            action: (req, res, next) =>
+              res.success({
+                msg: 'Welcome ! ',
+              }),
+          },
+        ],
+        '/healthcheck': [
+          {
+            method: 'get',
+            middlewares: [], // By default, this route is publicly available, you should create a middleware to protect this resource.
+            action: (req, res, next) => res.success({ msg: 'Pong !' }),
+          },
+        ],
+      },
+    },
+  },
+  resources: [
+    {
+      path: '/public',
+      resource: path.join(__dirname, '..', 'public'),
+    },
+  ],
+});
--- packages/webux-generator/generator/app-v2/templates/backend/config/security.js
+++ packages/webux-generator/generator/app-v2/templates/backend/config/security.js
@@ -1,17 +1,32 @@
 module.exports = {
-  origin: process.env.ORIGIN
-    ? process.env.ORIGIN.split(" ")
-    : ["http://localhost:8080", "http://127.0.0.1:8080"],
   bodyParser: {
-    limit: "3mb",
-    extended: true
+    limit: '1mb',
+    extended: false,
   },
   cookieParser: {
-    secret: process.env.COOKIE_SECRET || "SHUUUT!"
+    secret: process.env.COOKIE_SECRET || 'CookieSecret',
   },
-  trustProxy: true,
-  allowedMethods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
-  allowedCredentials: true,
-  allowedHeaders:
-    "Origin,X-Requested-with,Accept,Authorization,Content-Type,Accept-Language"
+  cors: {
+    whitelist: ['https://webuxlab.com', 'http://127.0.0.1'], // or [] to disable cors
+  },
+  server: {
+    trustProxy: true,
+    allowedMethods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    allowedCredentials: false,
+    allowedHeaders: 'Origin, X-Requested-with, Accept, Authorization, Content-Type, Accept-Language',
+  },
+  rateLimiters: [
+    {
+      name: 'Authentication',
+      time: 3600, // blocked for 1 hour
+      maxReq: 10, // after 3 tries
+      pattern: '/auth', // The route prefix to apply this limiter
+    },
+    {
+      name: 'Global',
+      time: 60, // blocked for 1 minute
+      maxReq: 150, // after 5 tries the requester will be blocked for 1 minute
+      pattern: '', // It applies globally
+    },
+  ],
 };
