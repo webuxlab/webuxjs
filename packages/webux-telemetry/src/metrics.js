49f0301f0648dcad339b8fc4dbf26ee2ef98a643
tommy gingras <tommy@studiowebux.com>
2023-03-01 00:31:27 -0500

    feat: added prometheus metrics and middlewares

---
+++ packages/webux-telemetry/src/metrics.js
@@ -0,0 +1,40 @@
+const promClient = require('prom-client');
+const promBundle = require('express-prom-bundle');
+
+module.exports = {
+  metricsMiddleware(projectName, serviceName, version = '0') {
+    return promBundle({
+      buckets: [0.1, 5, 15, 50, 100, 200, 300, 400, 500],
+      includeMethod: true,
+      includePath: true,
+      includeStatusCode: true,
+      includeUp: true,
+      customLabels: { project_name: projectName, service_name: serviceName, version },
+      metricsPath: '/metrics',
+      promClient: {
+        collectDefaultMetrics: {},
+      },
+    });
+  },
+
+  requestCounterMiddleware(projectName, serviceName, version = '0') {
+    const httpRequestsTotal = new promClient.Counter({
+      name: 'http_requests_total',
+      help: 'HTTP Requests Total',
+      labelNames: ['method', 'route', 'project_name', 'service_name', 'version'],
+    });
+
+    return (req, res, next) => {
+      httpRequestsTotal
+        .labels({
+          version,
+          method: req.method,
+          route: req.path,
+          project_name: projectName,
+          service_name: serviceName,
+        })
+        .inc(1);
+      return next();
+    };
+  },
+};
