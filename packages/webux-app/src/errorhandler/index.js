528c769faf7f4655d6d6cf25661d32b9d87dae31
tommy gingras <tommy@studiowebux.com>
2023-09-24 19:04:14 -0400

    feat: moved api error

--- packages/webux-app/src/errorhandler/index.js
+++ packages/webux-app/src/errorhandler/index.js
@@ -119,4 +119,16 @@ const Handler = (code, msg, extra, devMsg) => {
   return error;
 };
 
-module.exports = { Handler, GlobalHandler, NotFoundHandler };
+class ApiError extends Error {
+  constructor(message, name, code, extra, devMsg) {
+    super(message);
+
+    this.name = name || 'UNKNOWN_ERROR';
+    this.cause = name || 'UNKNOWN_ERROR';
+    this.code = code || 500;
+    this.extra = extra || {};
+    this.devMessage = devMsg || '';
+  }
+}
+
+module.exports = { Handler, GlobalHandler, NotFoundHandler, ApiError };
