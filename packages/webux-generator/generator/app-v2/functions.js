2d73cf181d265dc96f40861002a44318aef9a208
tommy gingras <tommy@studiowebux.com>
2023-09-24 19:28:27 -0400

    fix: generator app-v2

--- packages/webux-generator/generator/app-v2/functions.js
+++ packages/webux-generator/generator/app-v2/functions.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // ███████╗██╗   ██╗███╗   ██╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗███████╗
 // ██╔════╝██║   ██║████╗  ██║██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝
 // █████╗  ██║   ██║██╔██╗ ██║██║        ██║   ██║██║   ██║██╔██╗ ██║███████╗
@@ -12,108 +13,89 @@
  * License: All rights reserved Studio Webux 2015-Present
  */
 
-"use strict";
+const fs = require('fs');
+const path = require('path');
+const fse = require('fs-extra');
 
-const fs = require("fs");
-const path = require("path");
-const fse = require("fs-extra");
-
-let slash = "/";
-if (process.platform === "win32") {
-  slash = "\\";
+let slash = '/';
+if (process.platform === 'win32') {
+  slash = '\\';
 }
 
 const hasChildren = (files, base, parent, child, file) => {
   let filename = file ? path.join(file) : path.join(base);
-  if (
-    typeof parent[child] === "object" &&
-    Object.keys(parent[child]).length > 0
-  ) {
+  if (typeof parent[child] === 'object' && Object.keys(parent[child]).length > 0) {
     filename = path.join(filename, child);
 
-    Object.keys(parent[child]).forEach(element => {
+    Object.keys(parent[child]).forEach((element) => {
       hasChildren(files, base, parent[child], element, filename);
     });
   } else {
-    if (parent[child] && typeof parent[child] === "string") {
+    if (parent[child] && typeof parent[child] === 'string') {
       filename = path.join(filename, parent[child]);
     } else {
       filename = path.join(filename, child);
     }
     files.push(filename);
   }
-
-  return;
 };
 
 async function createFile(file, templatePath, projectDirectory) {
   return new Promise((resolve, reject) => {
-    try {
-      let sanitizeFile = file.replace(/\.\.\//g, ""); // remove pattern '../'
-      if (process.platform === "win32") {
-        sanitizeFile = file.replace(/\.\.\\/g, ""); // remove pattern '..\'
-      }
-      sanitizeFile = sanitizeFile.replace(templatePath, ""); // remove the template path
+    let sanitizeFile = file.replace(/\.\.\//g, ''); // remove pattern '../'
+    if (process.platform === 'win32') {
+      sanitizeFile = file.replace(/\.\.\\/g, ''); // remove pattern '..\'
+    }
+    sanitizeFile = sanitizeFile.replace(templatePath, ''); // remove the template path
 
-      fs.stat(path.join(projectDirectory, sanitizeFile), (err, exist) => {
-        if (err && err.code !== "ENOENT") {
-          reject(err);
-        } else if (exist) {
-          console.log(
-            path.join(projectDirectory, sanitizeFile),
-            `\x1b[33mFile exist\x1b[0m`
-          );
-          return resolve();
+    fs.stat(path.join(projectDirectory, sanitizeFile), (err, exist) => {
+      if (err && err.code !== 'ENOENT') {
+        reject(err);
+      } else if (exist) {
+        console.log(path.join(projectDirectory, sanitizeFile), `\x1b[33mFile exist\x1b[0m`);
+        return resolve();
+      }
+
+      const newDir = path.join(projectDirectory, sanitizeFile);
+      const dir = newDir.substr(0, newDir.lastIndexOf(slash));
+
+      return fse.ensureDir(dir, (ensureDirErr) => {
+        // if dir not exists, create it.
+        if (ensureDirErr) {
+          return reject(ensureDirErr);
         }
 
-        const newDir = path.join(projectDirectory, sanitizeFile);
-        const dir = newDir.substr(0, newDir.lastIndexOf(slash));
-
-        fse.ensureDir(dir, err => {
-          // if dir not exists, create it.
-          if (err) {
-            reject(err);
-          }
-
-          fse.copy(
-            path.join(file), // from
-            path.join(projectDirectory, sanitizeFile), //to
-            err => {
-              if (err) {
-                reject(err);
-              }
-
-              console.log(file, `\x1b[32mCopied !\x1b[0m`);
-              return resolve();
+        return fse.copy(
+          path.join(file), // from
+          path.join(projectDirectory, sanitizeFile), // to
+          (copyErr) => {
+            if (copyErr) {
+              reject(copyErr);
             }
-          );
-        });
+
+            console.log(file, `\x1b[32mCopied !\x1b[0m`);
+            return resolve();
+          },
+        );
       });
-    } catch (e) {
-      throw e;
-    }
+    });
   });
 }
-
 
 /**
  * For each required files, check if it already exist
  * Or copy it and replace the variable with the good values.
- * @param {*} files 
- * @param {*} templatePath 
- * @param {*} projectDirectory 
+ * @param {*} files
+ * @param {*} templatePath
+ * @param {*} projectDirectory
  */
 async function processFiles(files, templatePath, projectDirectory) {
-  try {
-    for (const dest of files) {
-      await createFile(dest, templatePath, projectDirectory).catch(e => {
-        throw e;
-      });
-    }
-    console.log("done");
-  } catch (e) {
-    throw e;
+  for await (const dest of files) {
+    await createFile(dest, templatePath, projectDirectory).catch((e) => {
+      throw e;
+    });
   }
+  console.log('done');
 }
 
 /**
@@ -121,14 +103,14 @@ async function processFiles(files, templatePath, projectDirectory) {
  * @param {String} dir The project path
  */
 function createGitignore(dir) {
-  console.log("Create .gitignore");
-  let data = "node_modules/\nlog/\n.DS_Store\n.tmp/\ndist/";
+  console.log('Create .gitignore');
+  const data = 'node_modules/\nlog/\n.DS_Store\n.tmp/\ndist/\n.env*';
 
-  return fs.writeFileSync(path.join(dir, "backend", ".gitignore"), data);
+  return fs.writeFileSync(path.join(dir, 'backend', '.gitignore'), data);
 }
 
 module.exports = {
   hasChildren,
   processFiles,
-  createGitignore
+  createGitignore,
 };
--- packages/webux-generator/generator/app-v2/index.js
+++ packages/webux-generator/generator/app-v2/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ████████╗ ██████╗ ██████╗
 // ██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
 // ██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║   ██║   ██║   ██║██████╔╝
@@ -12,63 +13,50 @@
  * License: All rights reserved Studio Webux 2015-Present
  */
 
-"use strict";
+const fs = require('fs');
+const path = require('path');
+const { prompt } = require('inquirer');
+const shell = require('shelljs');
 
-const fs = require("fs");
-const path = require("path");
-const { prompt } = require("inquirer");
-const { hasChildren, processFiles, createGitignore } = require("./functions");
-const { frontend, backend } = require("./structure");
-const { questions } = require("./questions");
-const { updateInfo } = require("../lib/utils");
-const shell = require("shelljs");
+const { hasChildren, processFiles, createGitignore } = require('./functions');
+const { frontend, backend } = require('./structure');
+const { questions } = require('./questions');
+const { updateInfo } = require('../lib/utils');
 
 try {
-  prompt(questions).then(answers => {
-    if (answers["validation"].toLowerCase() !== "y") {
+  prompt(questions).then((answers) => {
+    if (answers.validation.toLowerCase() !== 'y') {
       return;
     }
-    let projectDirectory = answers["projectDirectory"];
-    let projectName = answers["projectName"];
-    let projectAuthor = answers["projectAuthor"];
-    let projectDescription = answers["projectDescription"];
-    let templatePath = answers["templatePath"];
-    let files = [];
-
-    let projectPath = path.join(projectDirectory, projectName);
+    const { projectDirectory, projectName, projectAuthor, projectDescription, templatePath } = answers;
+    const files = [];
+    const projectPath = path.join(projectDirectory, projectName);
 
     if (!path.isAbsolute(projectDirectory)) {
-      throw new Error("The project directory must be an absolute path.");
+      throw new Error('The project directory must be an absolute path.');
     }
 
     if (!path.isAbsolute(templatePath)) {
-      throw new Error("The template directory must be an absolute path.");
+      throw new Error('The template directory must be an absolute path.');
     }
 
     const options = {
-      files: [
-        path.join(projectDirectory, projectName, "backend", "package.json")
-      ],
+      files: [path.join(projectDirectory, projectName, 'backend', 'package.json')],
       from: [/{{author}}/g, /{{description}}/g, /{{name}}/g],
-      to: [projectAuthor, projectDescription, projectName]
+      to: [projectAuthor, projectDescription, projectName],
     };
 
     // Create frontend architecture
-    Object.keys(frontend).forEach(element => {
-      hasChildren(
-        files,
-        path.join(templatePath, "frontend"),
-        frontend,
-        element
-      );
+    Object.keys(frontend).forEach((element) => {
+      hasChildren(files, path.join(templatePath, 'frontend'), frontend, element);
     });
 
     // Create backend architecture
-    Object.keys(backend).forEach(element => {
-      hasChildren(files, path.join(templatePath, "backend"), backend, element);
+    Object.keys(backend).forEach((element) => {
+      hasChildren(files, path.join(templatePath, 'backend'), backend, element);
     });
 
-    fs.mkdir(projectPath, err => {
+    fs.mkdir(projectPath, (err) => {
       if (err && err.errno !== -17) {
         throw err;
       }
@@ -76,26 +64,24 @@ try {
       Promise.all([processFiles(files, templatePath, projectPath)])
         .then(async () => {
           await createGitignore(projectPath);
-          console.log("Webux Generator - update info");
+          console.log('Webux Generator - update info');
           updateInfo(options)
             .then(() => {
-              console.log("Installing Dependencies ...");
+              console.log('Installing Dependencies ...');
               shell.cd(`${projectName}/backend`);
-              shell.exec("npm install");
-              console.log("-----------");
-              console.log("Getting Started: ");
+              shell.exec('npm install');
+              console.log('-----------');
+              console.log('Getting Started: ');
               console.log(`cd ${projectName}/backend`);
-              console.log("npm start");
-              console.log("-----------");
-              console.log(
-                "More details : http://www.webuxlab.com/docs/webux_demo/getting_started/"
-              );
+              console.log('npm start');
+              console.log('-----------');
+              console.log('More details : http://www.webuxlab.com/docs/webux_demo/getting_started/');
             })
-            .catch(e => {
+            .catch((e) => {
               throw e;
             });
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(`\x1b[31m${error}\x1b[0m`);
         });
     });
