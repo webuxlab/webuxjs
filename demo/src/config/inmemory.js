51ac73dcedec14dca1585ca5989810ff0a29d7a3
tommy gingras <tommy@studiowebux.com>
2024-06-19 19:47:48 -0400

    fix: fixing demo

--- demo/src/config/inmemory.js
+++ demo/src/config/inmemory.js
@@ -2,13 +2,13 @@ export default {
   // Read/Write Instance(s)
   api_key: {
     redis: {
-      url: ['redis://redis:6379'],
+      url: ['redis://demo-wl-redis:6379'],
     },
   },
   // Read Only Instance(s)
   api_key_read_only: {
     redis: {
-      url: ['redis://redis-readonly:6379'],
+      url: ['redis://demo-wl-redis-readonly:6379'],
     },
   },
   redis_cluster: {},
